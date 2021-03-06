3c498d683baa8037a3e3655885bf790c71901302
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-06 17:07:08 +0530

    Exercise 1

--- TCP_Exercise/tcp_ex-1.js
+++ TCP_Exercise/tcp_ex-1.js
@@ -18,7 +18,7 @@ client.connect(3000, '127.0.0.1', function() {
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+	client.destroy();
 });
 
 client.on('close', function() {
