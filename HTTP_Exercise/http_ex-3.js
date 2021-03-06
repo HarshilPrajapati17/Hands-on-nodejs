86d7a7b483d96279bc4d5403204773a826420eb7
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-06 15:33:56 +0530

    Exercise 3

--- HTTP_Exercise/http_ex-3.js
+++ HTTP_Exercise/http_ex-3.js
@@ -1,14 +1,12 @@
-
 var fs = require('fs');
 
-
 require('http').createServer(function(req, res) {
-var fileName = '/home/harshil/JS_Demo/Hands-on-nodejs/HTTP_Exercise/demo.txt';
-console.log("writing " + fileName);
-var writeStream = fs.createWriteStream(fileName);
-req.pipe(writeStream);
-req.on('end', function() {
-res.writeHead(200);
-res.end();
-});
+    var fileName = '/home/harshil/JS_Demo/Hands-on-nodejs/HTTP_Exercise/demo.txt';
+    console.log("writing " + fileName);
+    var writeStream = fs.createWriteStream(fileName);
+    req.pipe(writeStream);
+    req.on('end', function() {
+    res.writeHead(200);
+    res.end();
+    });
 }).listen(3000);
