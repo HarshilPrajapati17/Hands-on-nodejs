7ce4d07daecdea9f1d7409443bee635f58abbd51
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-09 14:02:31 +0530

    Exercise 3

---
+++ TLS_Exercise/tls_ex-3.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var client = require('tls').connect(4001, function(err) {
+    if(err){throw err;}
+client.connected = true;
+console.log('connected');
+process.stdin.resume();
+process.stdin.pipe(client);
+client.pipe(process.stdout, {end: false});
+});
