ee6675673a4399fb53446cb5f0d65c067be2e6cb
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-09 17:46:06 +0530

    Exercise 4

---
+++ TLS_Exercise/tls_ex-4.js
@@ -0,0 +1,14 @@
+var fs =require('fs');
+var options = {
+    key: fs.readFileSync(__dirname + '/tls_ex-2/server-key.pem'),
+    cert: fs.readFileSync(__dirname + '/tls_ex-2/server-cert.pem'),
+    ca: fs.readFileSync(__dirname + '/private/cakey.pem'),
+    requestCert: true,
+    rejectUnauthorized: true
+};
+require('tls').createServer(options, function(socket) {
+    socket.on('data', function(data) {
+    console.log(data.toString());
+});
+socket.pipe(socket);
+}).listen(4001);
