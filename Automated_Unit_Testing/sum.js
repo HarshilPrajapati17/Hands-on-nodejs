96ef89267f608ecad019bd152419c8410f48db04
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-11 14:07:46 +0530

    Main File

---
+++ Automated_Unit_Testing/sum.js
@@ -0,0 +1,24 @@
+require('should');
+var sum = require('./mod1');
+var asyn = require('./mod2');
+
+describe("Sum Func. and asynch. Test : \n", function(){
+    it('should be sum 0,5', function(){
+        sum(0,5).should.equal(5);
+    });
+``
+    it('should be sum 2,5', function(){
+        sum(2,5).should.equal(7);
+    });
+
+    it('should be sum 1,1 . 1,2 . 2,1 . 10,120 :', function(){
+        sum(1,1).should.equal(2);
+        sum(1,2).should.equal(3);
+        sum(2,1).should.equal(3);
+        sum(10,120).should.equal(130);
+    });
+
+    it('Testing Asynchronous function test : ', function(){
+        asyn.testAsync().should.equal(2);
+    })
+})
